73883388be11b42ba65de86dcef3b8364178dc08
Luica <138072443+lumartins311@users.noreply.github.com>
2023-09-05 19:11:25 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,7 @@
 
 import React, { useState, useEffect, useRef } from "react";
+import { faPlay, faPause, faPalette } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,29 +9,36 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 
-
-	const [canciones, setCanciones] = useState([])
+	const [selected, setSelected] = useState("")
+	const [canciones, setCanciones] = useState([])//en este guardo la cancion que tengamos seleccionada cuando hago click
 	const [Index, setIndex] = useState([])
+	const [url, setUrl] = useState("")// estado para la url que esta seleccionada
+
+	const [stateCancion, setStateCancion] = useState("Reproducir")//para cambiar el icono de reproducir
 	console.log(canciones);
 	console.log(Index);
 
 
-	const inputElement = useRef();
-	const nextElement = useRef();
+	const inputElement = useRef();//referencia elemento seleccionado
+
+	useEffect(function () {
+		console.log(url)
+		inputElement.current.play()
+		setStateCancion("Pausar")
+	}, [url])
+
+	// const focusInput = () => {
 
 
-	const focusInput = () => {
+	// 	inputElement.current.play();
 
+	// 	console.log(inputElement.current);
 
-		inputElement.current.play();
+	// };
+	// const focusNext = () => {
+	// 	nextElement.current.play();
 
-		console.log(inputElement.current);
-
-	};
-	const focusNext = () => {
-		nextElement.current.play();
-
-	}
+	// }
 
 
 
@@ -46,86 +55,73 @@ const Home = () => {
 		obtenerCanciones()
 	}, [])
 
-	function SeleccionarCancion(url) {
+	function SeleccionarCancion(url, index) {
 		console.log(url)
 		let urlsong = "https://assets.breatheco.de/apis/sound/" + url;
-		inputElement.current.src = urlsong;
+		setSelected(index)//???????
+		setUrl(urlsong)
+
 		//return urlsong
 		// const audio = 
 		// audio.play(url);
 
 	}
 	function next() {
-		if (Index == 19) {
-			inputElement.current.src = "https://assets.breatheco.de/apis/sound/" + canciones[0].url
-
-			setIndex(1)
-			inputElement.current.play()
+		if (Index == 18) {
+			setSelected(0)
 		}
-		// else (index < 20)
-		// index + 1
-		inputElement.current.src = "https://assets.breatheco.de/apis/sound/" + canciones[Index].url
-		setIndex(Index + 1)
+		else {
+			setSelected(selected + 1)
+		}
+		const song = canciones[selected].url//seleccionamos la cancion con este index
+		const urlsong = "https://assets.breatheco.de/apis/sound/" + song;
+		setUrl(urlsong)
 		inputElement.current.play()
-		// let urlsong = "https://assets.breatheco.de/apis/sound/" + url;
-		// let Next = urlsong[index]
-		// nextElement.current.src = Next
-		// nextElement.current.play();
-		console.log(canciones[Index].url)
-		console.log(Index)
+
 	}
 	function previus(url, index) {
-		if (index >= 0)
-			index - 1
-		let urlsong = "https://assets.breatheco.de/apis/sound/" + url;
-		let anterior = urlsong[index]
-		inputElement.current.src = anterior
+		if (index === 0) {
+			setSelected(18)
+		}
+		else {
+			setSelected(selected - 1)
+		}
+		const song = canciones[selected].url//seleccionamos la cancion con este index
+		const urlsong = "https://assets.breatheco.de/apis/sound/" + song;
+		setUrl(urlsong)
+		inputElement.current.play()
 	}
-	function ponerCancion(param) {
-		focusInput()
-		console.log("funcion")
-
+	function ponerCancion() {
+		if (stateCancion === "Reproducir") {
+			setStateCancion("Pausar")
+			inputElement.current.play()
+		} else {
+			inputElement.current.pause()
+			setStateCancion("Reproducir")
+		}
 	}
-	function playNext() {
-		next(focusNext())
-
-		console.log("aaaaaaaaaaaaa")
-	}
-
-
-
-	function playSong() {
-		canciones.forEach(setCanciones => {
-			console.log(setCanciones)
-		})
-	}
-
 
 
 	return (
 		<div className="" style={{ width: "600px" }}>
-
-			<ol className="text-left text-light fs-4 text lh-lg mx-3 position-absolute top-0 start-50 translate-middle-x bg-dark" style={{ width: "600px", paddingLeft: "100px" }}>
+			<ul className="text-left text-light fs-4 text lh-lg mx-3 position-absolute top-0 start-50 translate-middle-x bg-dark" style={{ width: "600px" }}>
 				{/* [<li>Morty Smith</li>,<li>Rick Sanchez</li> ] */}
-				{canciones.map(function (item, index) { return <li key={item.id} onClick={function () { SeleccionarCancion(item.url) }}>{item.name}</li> })}
-			</ol>
-			<div className="d-flex justify-content-center bg-dark position-fixed bottom-0 start-50 translate-middle-x w-100 p-3">
-				<button className=" bg-transparent border-0 mx-3" style={{ fontSize: "1.7rem", color: "white" }} onClick={previus}><i className="fas fa-backward" /></button>
-				<audio ref={inputElement} src={focusInput}>
-
-				</audio>
-				<button className=" bg-transparent border-0 mx-3" style={{ fontSize: "1.7rem", color: "white" }} onClick={ponerCancion}><i className="fas fa-play" /> </button>
-				<audio ref={inputElement} src={focusInput}>
-
-				</audio>
-				<button className=" bg-transparent border-0 mx-3" style={{ fontSize: "1.7rem", color: "white" }} onClick={next}><i className="fas fa-forward" /></button>
-				<audio ref={inputElement} src={focusNext}>
-
-				</audio>
+				{canciones.map(function (item, index) {
+					return <li role="button" key={item.id} className={`d-flex flex-row opacity-${selected === index ? `75` : `100`}`} style={{ textDecoration: "underline" }} onClick={() => SeleccionarCancion(item.url, index)}>
+						<p>{index + 1}</p>
+						<p className="mx-3">{item.name}</p>
+					</li>
+				})}
+			</ul>
+			<div className="bg-dark position-fixed bottom-0 start-50 translate-middle-x w-100 p-3">
+				<audio ref={inputElement} src={url} ></audio>
+				<button className="" onClick={previus}>anterior</button>
+				<button className="" onClick={ponerCancion}>{stateCancion === "Reproducir" ? <FontAwesomeIcon icon={faPlay} /> : <FontAwesomeIcon icon={faPause} />}</button>
+				<button className="" onClick={next}>next</button>
 			</div>
 		</div>
 
 	);
-};
+}
 
 export default Home;
